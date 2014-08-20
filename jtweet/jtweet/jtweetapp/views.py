f9adc1e741c660b2d232c613efc969d9fabd9601
Heissen Lopez <heissen.lopez@gmail.com>
2014-08-19 20:32:03 -0500

    Add views config app

--- jtweet/jtweet/jtweetapp/views.py
+++ jtweet/jtweet/jtweetapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
+
+def index(request):
+  return render_to_responser('jtweetapp/index.html',
+      {},
+      context_instance=RequestContext(request))
