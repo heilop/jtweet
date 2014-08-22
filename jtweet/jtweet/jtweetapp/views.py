f5c56ce9e5935262d742e912842aec2b7170516e
Heissen Lopez <heissen.lopez@gmail.com>
2014-08-22 10:32:53 -0500

    Declaring jtweetapp in settings file

--- jtweet/jtweet/jtweetapp/views.py
+++ jtweet/jtweet/jtweetapp/views.py
@@ -1,11 +1,9 @@
-#from django.shortcuts import render_to_response
-from django.shortcuts import render
-#from django.template import RequestContext
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
+
 def index(request):
-  return HttpResponse('Custom index message')
-  # return render_to_response('jtweetapp/index.html',
-  # {},
-  # context_instance=RequestContext(request))
+  return render_to_response('jtweetapp/index.html',
+      {},
+      context_instance=RequestContext(request))
